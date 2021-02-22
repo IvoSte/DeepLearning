6122a078de13d380d45936d4ae112a60b908895f
marieke <jdsfkl>
2021-02-22 23:51:19 +0100

    lenet werkt! verder beetje geleerd en gecomment

--- lenet.py
+++ lenet.py
@@ -4,32 +4,40 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# Normalize data: probably TODO change
-(train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
-train_x = train_x / 255.0
-test_x = test_x / 255.0
-train_x = tf.expand_dims(train_x, 3)
-test_x = tf.expand_dims(test_x, 3)
-val_x = train_x[:5000]
-val_y = train_y[:5000]
+def lenet_model():
+    (train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
+    print(len(train_x), len(train_y), len(test_x), len(test_y))
 
-# Actual model
-lenet_5_model = keras.models.Sequential([
-    keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
-    keras.layers.AveragePooling2D(), #S2
-    keras.layers.Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
-    keras.layers.AveragePooling2D(), #S4
-    keras.layers.Flatten(), #Flatten
-    keras.layers.Dense(120, activation='tanh'), #C5
-    keras.layers.Dense(84, activation='tanh'), #F6
-    keras.layers.Dense(10, activation='softmax') #Output layer
-])
+    # Normalize pixel intensities
+    train_x = train_x / 255.0
+    test_x = test_x / 255.0
 
-# Compile; uses Adam, so not original
-lenet_5_model.compile(optimizer=’adam’, loss=keras.losses.sparse_categorical_crossentropy, metrics=[‘accuracy’])
+    # # 
+    train_x = tf.expand_dims(train_x, 3)
+    test_x = tf.expand_dims(test_x, 3)
+    val_x = train_x[:5000]
+    val_y = train_y[:5000]
+    # Actual model
+    lenet_5_model = keras.models.Sequential([
+        keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
+        keras.layers.AveragePooling2D(), #S2
+        keras.layers.Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
+        keras.layers.AveragePooling2D(), #S4
+        keras.layers.Flatten(), #Flatten
+        keras.layers.Dense(120, activation='tanh'), #C5
+        keras.layers.Dense(84, activation='tanh'), #F6
+        keras.layers.Dense(10, activation='softmax') #Output layer
+    ])
 
-# Fit
-lenet_5_model.fit(train_x, train_y, epochs=5, validation_data=(val_x, val_y))
+    # Compile; uses Adam, so not original
+    lenet_5_model.compile(optimizer='adam', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
 
-# Evaluate: this outputs
-lenet_5_model.evaluate(test_x, test_y)
+    # Fit
+    print("fitting...")
+    lenet_5_model.fit(train_x, train_y, epochs=5, validation_data=(val_x, val_y))
+
+    print("evaluating...")
+    # Evaluate: this outputs
+    lenet_5_model.evaluate(test_x, test_y)
+
+lenet_model()
--- prep.py
+++ prep.py
@@ -3,47 +3,21 @@ import shutil
 import numpy as np
 from PIL import Image
 
-# print(len(os.listdir("data")))
-
-# def get_image_arrays(src_path, dest_path, color_mode, dim):
-#     cls, lab = get_class_labels(src_path)
-#     copy_images_to_folders(src_path, dest_path)
-#     data = []
-#     labels =[]
-#     for class_, label in zip(cls, lab):
-#         for files in os.listdir(dest_path):
-#             dir_name = os.path.basename(str(dest_path +'/'+files))
-#             if dir_name.lower() == class_:
-#                 for f in os.listdir(dest_path + '/' + dir_name):
-#                     if f.split('.')[-1] == 'jpg':
-#                             img = Image.open(dest_path +'/'+class_+'/'+f)
-#                             img_cs = img.convert(color_mode)
-#                             img_resize = img_cs.resize(new_dim)
-#                             img_array = np.asarray(img_resize)
-#                             data.append(img_array)
-#                             labels.append(label)
+# Import an image from path, print specs, show image
+def open_print_specs(im_path, show_im):
+    im = Image.open(im_path)
+    print("Format:", im.format, 
+            "\n Mode:", im.mode, 
+            "\n Size:", im.size)
+    if show_im:
+        im.show()
     
-#     img_X = np.array(data)
-#     img_y = np.asarray(labels, dtype='uint8')
-   
-#     print(
-#         'Shape of X:\n', img_X.shape,
-#         '\nShape of y:\n', len(img_y),
-#         '\nPrint label:\n', img_y
-#     )
-#     return img_X,img_y
-
-# DIM = 100
-# COLOR_MODE = 'L'
-
-# img_X, img_y = get_image_arrays(
-#     src_path,
-#     dest_path,
-#     COLOR_MODE,
-#     DIM
-# )
-
-def import_image(image_path):
+    im_arr = np.array(im)
+    print("Pixel intensities: \n", im_arr)
+    return im_arr
+    
+# Get array of pixels from image path
+def get_arr_image(image_path):
     # with Image.open(image_path) as image:
     #     im_arr = np.fromstring(image.tobytes(), dtype=np.uint8)
     #     print(image.size)
@@ -51,8 +25,29 @@ def import_image(image_path):
     print(im_arr)
     return im_arr
 
+# Generate image from array version of test.jpg, save to given image path
+# Also, generate image from given array. Return both
 def generate_image(image_path, im_arr):
     image2 = Image.fromarray(im_arr)
     resized = np.array(Image.open("data/train/class_2/test.jpg").resize((200,200)))
     Image.fromarray(resized).save(image_path)
+
     return image2, resized
+
+# Normalizes pixel array with a bunch of checks
+def norm_pixels(pixels):
+    # confirm pixel range is 0-255
+    print('Data Type: %s' % pixels.dtype)
+    print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+    # convert from integers to floats
+    pixels = pixels.astype('float32')
+    # normalize to the range 0-1
+    pixels /= 255.0
+    # confirm the normalization
+    print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+    return pixels
+
+# Bit of scratch main code
+im_arr = open_print_specs("data/train/class_2/gif_first_frame_11aa (1).jpg", False)
+pix = norm_pixels(im_arr)
+print(pix)
