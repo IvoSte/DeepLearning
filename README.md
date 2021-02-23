cf53838b901fd21c46556c5551d0d4c04337c9fb
marieke <jdsfkl>
2021-02-23 23:16:05 +0100

    niet heel happy met deze progress

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DeepLearning
 Deep Learning Course
+
+# Files
+- ```data_prep.py```: helper functions for data preparation, including three ways of retrieving the data from the directory.
+- ```model.py```: currently used to train inception model
+- ```resnet.py```: probably redundant and not-tested implementation of resnet
+- ```lenet_old.py```: implementation of lenet that works perfectly
+- ```lenet.py```: so-far-failed attempts at providing lenet with input that hasn't been specified as x and y.
--- lenet.py
+++ lenet.py
@@ -3,47 +3,44 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import data_prep
 
 def data_mnist():
-    (train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
-    print(len(train_x), len(train_y), len(test_x), len(test_y))
-    print(type(train_x[1]))
+    train_ds, test_ds = keras.datasets.mnist.load_data()
+    print(type(train_ds)) # = np array
+    for t in train_ds:
+        print("type: ", type(t)) # = np array
 
-    # Normalize pixel intensities
-    train_x = train_x / 255.0
-    test_x = test_x / 255.0
+        # Normalize pixel intensities
+        t = t / 255.0
+    
+    for t in test_ds:
+        print("type: ", type(t)) # = np array
 
-    # # 
-    train_x = tf.expand_dims(train_x, 3)
-    test_x = tf.expand_dims(test_x, 3)
-    val_x = train_x[:5000]
-    val_y = train_y[:5000]
-    return train_x, train_y, test_x, test_y, val_x, val_y
+        # Normalize pixel intensities
+        t = t / 255.0
+
+    train_ds = tf.expand_dims(train_ds[0], 3)
+    test_ds = tf.expand_dims(test_ds[0], 3)
+    val_ds = train_ds[:5000]
+
+    print(type(train_ds), type(test_ds), type(val_ds))
+    return train_ds, val_ds, test_ds
 
 def our_data():
-    # train_x = numpy.ndarray
-    # for i in range(0,6):
-    #     train_x
-    (train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
-    print(len(train_x), len(train_y), len(test_x), len(test_y))
-    print(type(train_x[1]))
+    # (train_ds, val_ds, test_ds) = data_prep.gen_data_2()
+    (train_ds, val_ds, test_ds) = data_prep.gen_data_1()
 
-    # Normalize pixel intensities
-    train_x = train_x / 255.0
-    test_x = test_x / 255.0
+    # TODO normalize, expand dims
 
-    # # 
-    train_x = tf.expand_dims(train_x, 3)
-    test_x = tf.expand_dims(test_x, 3)
-    val_x = train_x[:5000]
-    val_y = train_y[:5000]
-    return train_x, train_y, test_x, test_y, val_x, val_y
+    return train_ds, val_ds, test_ds
 
+def run_lenet(train_ds, val_ds, test_ds):
 
-def run_lenet(train_x, train_y, test_x, test_y, val_x, val_y):
     # Actual model
+    '''Changed input_shape in order to not need train_x. Same values tho'''
     lenet_5_model = keras.models.Sequential([
-        keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=train_x[0].shape, padding='same'), #C1
+        keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=(28, 28, 1), padding='same'), #C1
         keras.layers.AveragePooling2D(), #S2
         keras.layers.Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'), #C3
         keras.layers.AveragePooling2D(), #S4
@@ -53,17 +50,22 @@ def run_lenet(train_x, train_y, test_x, test_y, val_x, val_y):
         keras.layers.Dense(10, activation='softmax') #Output layer
     ])
 
-    # Compile; uses Adam, so not original
+    # Compile; uses Adam, so not original? 
     lenet_5_model.compile(optimizer='adam', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-
+    
     # Fit
     print("fitting...")
-    lenet_5_model.fit(train_x, train_y, epochs=5, validation_data=(val_x, val_y))
+    lenet_5_model.fit(train_ds, batch_size=5, validation_data=val_ds, epochs=5)
 
+    # Evaluate
     print("evaluating...")
-    # Evaluate: this outputs
-    lenet_5_model.evaluate(test_x, test_y)
+    lenet_5_model.evaluate(test_ds)
+
 
 # Main part: get data as tuple and pass it to runner
-m_data = data_mnist()
-run_lenet(*m_data)
+if input("Would you like to use the our data? Default is MNIST. y/[n] ") == 'y':
+    print("OK, using our data.")
+    run_lenet(*our_data())
+else:
+    print("OK, using MNIST data.")
+    run_lenet(*data_mnist())
